e7f18864c1ada2c7aa21ea73bf6ef052361657ea
Tauheed Shahid <tauheedshahid@gmail.com>
2020-10-17 15:47:43 +0530

    modified home.ejs,mocktest.ejs

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const mongoose= require('mongoose');
 const connectDB = async ()=>{
     try{
         const conn = await mongoose.connect(process.env.MONGO_URI,{
+            // const conn = await mongoose.connect("mongodb+srv://admin:Admin123@sei.cence.mongodb.net/SomnathEducationInstitute?retryWrites=true&w=majority",{
             useNewUrlParser:true,
             useUnifiedTopology:true,
             useFindAndModify:false
